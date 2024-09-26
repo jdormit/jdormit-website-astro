55f44678e03289c54515823737bed4cfb9b81fad
jdormit <jeremy.dormitzer@gmail.com>
2024-09-26 10:54:57 -0400

    Add post content & description to RSS feed, add RSS discovery link

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import excerptHtml from "excerpt-html";
+import { marked } from "marked";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
@@ -8,9 +11,15 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts.map((post) => {
+      let html = marked.parse(post.body);
+      let excerpt = excerptHtml(html, { pruneLength: 500 });
+      return {
+        ...post.data,
+        content: sanitizeHtml(html),
+        description: sanitizeHtml(excerpt),
+        link: `/blog/${post.slug}/`,
+      };
+    }),
   });
 }
